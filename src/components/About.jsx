95d890d3dec6e57937a8f24b5214b18b519fb3a0
Anish Maharjan <anishmhn@hotmail.com>
2024-12-31 14:13:20 +0200

    added info in about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,10 +21,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I am fourth-year IT student in Metropolia University of Applied Sciences, 
+        I am a fourth-year IT student in Metropolia University of Applied Sciences, 
         currently learning and gaining knowledge in various programming languages and technologies. Since August 2023, 
         I have been developing my skills as junior software developer at TOYOTA GAZOO Racing World Rally Team Oy, 
-        where I apply my skills to real-world projects in a dynamic environment.
+        where I apply my skills to real-world projects in a dynamic environment. I am eager to continue learning and 
+        leveraging my skills to create impactful solutions in the tech industry.
       </motion.p>
     </motion.section>
   );
